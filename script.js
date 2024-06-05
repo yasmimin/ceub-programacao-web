c8502e5455914b1d82003c2bb650c474dfbfd372
Yasmin <142934873+yasmimin@users.noreply.github.com>
2024-06-05 15:23:36 -0300

    commit 1

---
+++ script.js
@@ -0,0 +1,72 @@
+function soma(numero1, numero2) {
+    return numero1 + numero2;
+}
+
+let resultadoDaSoma = soma(2, 4);
+console.log(resultadoDaSoma);
+
+
+
+
+//let conteudo;
+
+//conteudo = document.getElementById("titulos");
+
+let conteudo_classe;
+conteudo_classe = document.querySelector(".paragrafo-classe");
+
+//console.log(conteudo_classe);
+
+//conteudo.innerHTML = "<marquee>Esse conteudo está vindo do javascript</marquee>"
+
+//let conteudo2 = document.getElementById("paragrafo-legal");
+//conteudo2.innerHTML = "<p>esse paragrafo vem do js</p>"
+
+//console.log(typeof(conteudo));
+//console.log(conteudo);
+
+
+
+
+
+
+
+// imprima os numeros de 1 a 5 na tela
+//for (let i = 1; i < 1000000; i += 1) {
+//    console.log(i);
+//}
+/*
+let i = 1;
+
+while(i < 6) {
+    console.log(i);
+    i++;
+}
+
+document.
+*/
+
+/*
+let idade = 17;
+let nome = "joao";
+let altura = 1.80;
+
+if (idade >= 18 || nome != "pedro") {
+    console.log("ola!");
+}
+
+let meuArray = [1, 3, 4, 6, 0, -3];
+
+console.log(meuArray[0]);
+console.log(meuArray);
+
+console.log(meuArray.length)
+
+let pessoa = {
+    nome: "joao",
+    idade: 18,
+    altura: 1.78
+} 
+
+console.log("a idade da pessoa " + pessoa['nome'] + " é " + pessoa.idade);
+*/
